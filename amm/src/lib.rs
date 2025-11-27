5d34dc01994ec85d41d335a02cfe499262283c22
ben-pekurovski <ben@yieldback.cash>
2025-11-27 16:04:11 -0600

    fresh code

--- amm/src/lib.rs
+++ amm/src/lib.rs
@@ -7,9 +7,3 @@ mod test;
 pub use contract::LiquidityPool;
 
 use soroban_sdk::contractmeta;
-
-// Metadata that is added on to the WASM custom section
-contractmeta!(
-    key = "Description",
-    val = "Constant product AMM"
-);
